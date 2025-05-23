482ce84761c26ee3161b6a6153b9329707574dd8
sinna25 <sinna202505@gmail.com>
2025-05-23 16:48:06 +0900

    Update Intro page script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-https://sinna25.github.io/odranywere-site/
+function enterSite() {
+  const intro = document.getElementById('intro');
+  const main = document.getElementById('main');
+
+  intro.style.opacity = '0';
+  setTimeout(() => {
+    intro.style.display = 'none';
+    main.style.display = 'block';
+    main.style.opacity = '1';
+  }, 500);
+}
